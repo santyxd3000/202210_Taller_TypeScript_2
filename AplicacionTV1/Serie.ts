f625a7428ba04da26821e1fcf65e3037b4a9f810
santyxd3000 <78179214+santyxd3000@users.noreply.github.com>
2022-04-21 12:53:22 -0500

    CommitFinal

---
+++ AplicacionTV1/Serie.ts
@@ -0,0 +1,22 @@
+//Otros tipos de datos para adelantar el taller 2
+export class Serie {
+    id: number;
+    name: string;
+    channel: string;
+    seasons: number;
+    description: string;
+    website: string;
+    image: string;
+
+
+    constructor(id: number,name:string,channel:string,seasons:number,description:string, website:string,
+        image: string){
+    this.id = id;
+    this.name = name;
+    this.channel = channel;
+    this.seasons = seasons;
+    this.description = description;
+    this.website = website;
+    this.image = image;
+    }
+}
